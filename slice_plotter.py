61994922eadab8028ec967772cb282802a4d0fc0
Gavin Day <mngad@leeds.ac.uk>
2018-02-07 16:55:02 +0000

    pep

--- slice_plotter.py
+++ slice_plotter.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     PCName = ''
     fig = plt.figure(1)
     filelist = []
-    allfilelist=sorted(os.listdir(sys.argv[1]))
+    allfilelist = sorted(os.listdir(sys.argv[1]))
     for filename in allfilelist:
         if filename.endswith(".stl"):
             filelist.append(filename)
 
     print(len(filelist))
-    if sys.argv[2] =='True':
-        myorder=[2,1,0,6,3,4,5]
-        filelist = [ filelist[i] for i in myorder]
+    if sys.argv[2] == 'True':
+        myorder = [2, 1, 0, 6, 3, 4, 5]
+        filelist = [filelist[i] for i in myorder]
 
     for filename in filelist:
         if filename.endswith(".stl"):
@@ -24,7 +24,11 @@ if __name__ == "__main__":
             vbmesh_un = su.get_uniq_pc(filename)
             midarrayz = su.get_mid_array(vbmesh_un, 1, 'z')
 
-            plt.scatter(midarrayz[:,0],midarrayz[:,1], s=1, label=filename[:-4].replace('_',' '))
+            plt.scatter(
+                midarrayz[:, 0],
+                midarrayz[:, 1],
+                s=1,
+                label=filename[:-4].replace('_', ' '))
 
     plt.xlabel('X axis')
     plt.ylabel('Y')
@@ -39,7 +43,11 @@ if __name__ == "__main__":
             vbmesh_un = su.get_uniq_pc(filename)
             midarrayz = su.get_mid_array(vbmesh_un, 1, 'x')
 
-            plt.scatter(midarrayz[:,1],midarrayz[:,2], s=1, label=filename[:-4].replace('_',' '))
+            plt.scatter(
+                midarrayz[:, 1],
+                midarrayz[:, 2],
+                s=1,
+                label=filename[:-4].replace('_', ' '))
 
     plt.xlabel('Y')
     plt.ylabel('Z axis')
@@ -54,11 +62,14 @@ if __name__ == "__main__":
             vbmesh_un = su.get_uniq_pc(filename)
             midarrayz = su.get_mid_array(vbmesh_un, 1, 'y')
 
-            plt.scatter(midarrayz[:,0],midarrayz[:,2], s=1, label=filename[:-4].replace('_',' '))
+            plt.scatter(
+                midarrayz[:, 0],
+                midarrayz[:, 2],
+                s=1,
+                label=filename[:-4].replace('_', ' '))
 
     plt.xlabel('X axis')
     plt.ylabel('Z axis')
     plt.legend()
     plt.savefig(PCName + '_Coronal')
     plt.show()
-
