7e5a273d15a3eda7ccded10e8eaf76b9e2043386
Gavin Day <mngad@leeds.ac.uk>
2018-02-07 16:53:04 +0000

    added slice_plotter to plot medial, coronal and axial slices

--- slice_plotter.py
+++ slice_plotter.py
@@ -1,15 +1,70 @@
 import os
 import sys
-import stl_uni as su
+import stl_tools as su
+import matplotlib.pyplot as plt
 
-
-
+def plot_slice(pointarray):
+    fig = plt.figure(1)
+    plt.subplot(331)
+    plt.scatter(pointarray[:,0],pointarray[:,1])
+    plt.show()
 
 if __name__ == "__main__":
     os.chdir(sys.argv[1])
-    for filename in sorted(os.listdir(sys.argv[1])):
+    PCName = ''
+    fig = plt.figure(1)
+    filelist = []
+    allfilelist=sorted(os.listdir(sys.argv[1]))
+    for filename in allfilelist:
+        if filename.endswith(".stl"):
+            filelist.append(filename)
+
+    print(len(filelist))
+    if sys.argv[2] =='True':
+        myorder=[2,1,0,6,3,4,5]
+        filelist = [ filelist[i] for i in myorder]
+
+    for filename in filelist:
+        if filename.endswith(".stl"):
+            PCName = filename[:3]
+            vbmesh_un = su.get_uniq_pc(filename)
+            midarrayz = su.get_mid_array(vbmesh_un, 1, 'z')
+
+            plt.scatter(midarrayz[:,0],midarrayz[:,1], s=1, label=filename[:-4].replace('_',' '))
+
+    plt.xlabel('X axis')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.savefig(PCName + '_Axial')
+    plt.show()
+    fig = plt.figure(2)
+
+    for filename in filelist:
         if filename.endswith(".stl"):
 
             vbmesh_un = su.get_uniq_pc(filename)
-            midarray, centreX, centreY, mid, x, y, z = measure_vb(
-                vbmesh_un, filename[:-4])
+            midarrayz = su.get_mid_array(vbmesh_un, 1, 'x')
+
+            plt.scatter(midarrayz[:,1],midarrayz[:,2], s=1, label=filename[:-4].replace('_',' '))
+
+    plt.xlabel('Y')
+    plt.ylabel('Z axis')
+    plt.legend()
+    plt.show()
+    plt.savefig(PCName + '_Sagital')
+    fig = plt.figure(3)
+
+    for filename in filelist:
+        if filename.endswith(".stl"):
+
+            vbmesh_un = su.get_uniq_pc(filename)
+            midarrayz = su.get_mid_array(vbmesh_un, 1, 'y')
+
+            plt.scatter(midarrayz[:,0],midarrayz[:,2], s=1, label=filename[:-4].replace('_',' '))
+
+    plt.xlabel('X axis')
+    plt.ylabel('Z axis')
+    plt.legend()
+    plt.savefig(PCName + '_Coronal')
+    plt.show()
+
