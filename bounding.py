5f6feec68e8b86c21fba91a10e56a1c0079e0c68
Gavin Day <mengda@leeds.ac.uk>
2020-08-17 13:34:14 +0100

    formatting

--- bounding.py
+++ bounding.py
@@ -1,15 +1,15 @@
 import stl_tools as su
-from mpl_toolkits import mplot3d
+# from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 import os
 import sys
 
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
-def cuboid_data2(o, size=(1,1,1)):
+def cuboid_data2(o, size=(1, 1, 1)):
     X = [[[0, 1, 0], [0, 0, 0], [1, 0, 0], [1, 1, 0]],
          [[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0]],
          [[1, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]],
@@ -18,30 +18,35 @@ def cuboid_data2(o, size=(1,1,1)):
          [[0, 1, 1], [0, 0, 1], [1, 0, 1], [1, 1, 1]]]
     X = np.array(X).astype(float)
     for i in range(3):
-        X[:,:,i] *= size[i]
+        X[:, :, i] *= size[i]
     X += np.array(o)
     return X
 
-def plotCubeAt2(positions,sizes=None,colors=None, **kwargs):
-    if not isinstance(colors,(list,np.ndarray)): colors=["C0"]*len(positions)
-    if not isinstance(sizes,(list,np.ndarray)): sizes=[(1,1,1)]*len(positions)
+
+def plotCubeAt2(positions, sizes=None, colors=None, **kwargs):
+    if not isinstance(colors, (list, np.ndarray)):
+        colors = ["C0"] * len(positions)
+    if not isinstance(sizes, (list, np.ndarray)):
+        sizes = [(1, 1, 1)] * len(positions)
     g = []
-    for p,s,c in zip(positions,sizes,colors):
-        g.append( cuboid_data2(p, size=s) )
+    for p, s, c in zip(positions, sizes, colors):
+        g.append(cuboid_data2(p, size=s))
     return Poly3DCollection(np.concatenate(g),
-                            facecolors=np.repeat(colors,6), alpha=0.2, **kwargs)
+                            facecolors=np.repeat(colors, 6),
+                            alpha=0.2,
+                            **kwargs)
+
 
 def plot(mesh_un, fname, folder):
 
     fig = pyplot.figure()
     ax = fig.add_subplot(111, projection='3d')
-    #= elliptical cylinder about z-axis 1
+    # = elliptical cylinder about z-axis 1
 
-    #ax.plot_surface(x, y, z, linewidth=1, color='red', alpha=0.7)
-
+    # ax.plot_surface(x, y, z, linewidth=1, color='red', alpha=0.7)
 
     ax.scatter(mesh_un[:, 0], mesh_un[:, 1], mesh_un[:, 2], s=1)
-    #ax.scatter(centreX, centreY, mid, s=5, c='g')
+    # ax.scatter(centreX, centreY, mid, s=5, c='g')
 
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
@@ -51,23 +56,25 @@ def plot(mesh_un, fname, folder):
     # positions = [(-3,5,-2),(1,7,1)]
     # sizes = [(4,5,3), (3,3,7)]
     # colors = ["crimson","limegreen"]
-    mesh_left_half = su.get_half_points(mesh_un, 0, su.get_extent(mesh_un, 0), 0)
-    mesh_right_half = su.get_half_points(mesh_un, 1, su.get_extent(mesh_un,0), 0)
+    mesh_left_half = su.get_half_points(mesh_un, 0, su.get_extent(mesh_un, 0),
+                                        0)
+    mesh_right_half = su.get_half_points(mesh_un, 1, su.get_extent(mesh_un, 0),
+                                         0)
 
     positions = [
         # su.boundingorigin(mesh_un),
         su.boundingorigin(mesh_left_half),
         su.boundingorigin(mesh_right_half)
-        ]
+    ]
     sizes = [
         # su.boundingsize(mesh_un),
         su.boundingsize(mesh_left_half),
         su.boundingsize(mesh_right_half)
-        ]
+    ]
     print("positions = " + str(positions))
     print("sizes = " + str(sizes))
     colors = ["crimson", "green"]
-    pc = plotCubeAt2(positions,sizes,colors=colors, edgecolor="k")
+    pc = plotCubeAt2(positions, sizes, colors=colors, edgecolor="k")
     ax.add_collection3d(pc)
 
     pyplot.savefig(folder + "/" + fname + '_persp.png')
@@ -82,9 +89,6 @@ def plot(mesh_un, fname, folder):
     ax.view_init(90, 0)
 
     pyplot.savefig(folder + "/" + fname + '_90_0.png')
-
-
-
 
 
 if __name__ == "__main__":
