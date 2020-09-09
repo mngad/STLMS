398b142b33d5451840f5f40fef14815c6d851ab5
Gavin Day <mengda@leeds.ac.uk>
2020-09-09 14:23:14 +0100

    plotting the means of the measurements with std

--- bounding.py
+++ bounding.py
@@ -35,21 +35,8 @@ def plotCubeAt2(positions, sizes=None, colors=None, **kwargs):
                             **kwargs)
 
 
-def plot(mesh_un, fname, folder):
+def get_mesh_parts(mesh_un, fname, folder):
 
-    fig = pyplot.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.scatter(mesh_un[:, 0], mesh_un[:, 1], mesh_un[:, 2], s=1)
-
-    ax.set_xlabel('X axis')
-    ax.set_ylabel('Y axis')
-    ax.set_zlabel('Z axis')
-    # rotate the axes and update
-
-    # positions = [(-3,5,-2),(1,7,1)]
-    # sizes = [(4,5,3), (3,3,7)]
-    # colors = ["crimson","limegreen"]
     mesh_left_half = su.get_half_points(mesh_un, 0, su.get_extent(mesh_un, 0),
                                         0)
     mesh_right_half = su.get_half_points(mesh_un, 1, su.get_extent(mesh_un, 0),
@@ -92,66 +79,110 @@ def plot(mesh_un, fname, folder):
     mesh_right_cond_thirds_middline = []
     for i in mesh_right_cond_thirds:
         mesh_right_cond_thirds_middline.append(su.get_mid_array(i, 2, "y"))
+
+    all_measurments = []
+
+    all_measurments.append(get_measurements(mesh_un_middline, "FCW", 0))
+    all_measurments.append(get_measurements(mesh_un_thirds_middline[2], "FCWA",
+                                            0))
+    all_measurments.append(get_measurements(mesh_un_thirds_middline[0], "FCWP",
+                                            0))
+    all_measurments.append(get_measurements(mesh_left_cond_thirds_middline[1],
+                                            "Left FW", 0))
+    all_measurments.append(get_measurements(mesh_left_cond_thirds_middline[2],
+                                            "Left FWA", 0))
+    all_measurments.append(get_measurements(mesh_left_cond_thirds_middline[0],
+                                            "Left FWP", 0))
+    all_measurments.append(get_measurements(mesh_left_cond_thirds_x_middline[1],
+                                            "Left FL", 1))
+    all_measurments.append(get_measurements(mesh_left_cond_thirds_x_middline[2],
+                                            "Left FLM", 1))
+    all_measurments.append(get_measurements(mesh_left_cond_thirds_x_middline[0],
+                                            "Left FLL", 1))
+    all_measurments.append(get_measurements(mesh_right_cond_thirds_middline[1],
+                                            "Right FW", 0))
+    all_measurments.append(get_measurements(mesh_right_cond_thirds_middline[2],
+                                            "Right FWA", 0))
+    all_measurments.append(get_measurements(mesh_right_cond_thirds_middline[0],
+                                            "Right FWP", 0))
+    all_measurments.append(get_measurements(mesh_right_cond_thirds_x_middline[1],
+                                            "Right FL", 1))
+    all_measurments.append(get_measurements(mesh_right_cond_thirds_x_middline[2],
+                                            "Right FLM", 1))
+    all_measurments.append(get_measurements(mesh_right_cond_thirds_x_middline[0],
+                                            "Right FLL", 1))
+
+    export_to_csv(fname, folder, all_measurments)
+
+
+def plot(mesh_un, fname, folder):
+
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(mesh_un[:, 0], mesh_un[:, 1], mesh_un[:, 2], s=1)
+
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    # rotate the axes and update
+
+    # positions = [(-3,5,-2),(1,7,1)]
+    # sizes = [(4,5,3), (3,3,7)]
+    # colors = ["crimson","limegreen"]
+    mesh_left_half = su.get_half_points(mesh_un, 0, su.get_extent(mesh_un, 0),
+                                        0)
+    mesh_right_half = su.get_half_points(mesh_un, 1, su.get_extent(mesh_un, 0),
+                                         0)
+    mesh_right_cond = su.get_condyle(mesh_right_half,
+                                     su.get_extent(mesh_right_half, 1),
+                                     0)
+
+    mesh_left_cond = su.get_condyle(mesh_left_half,
+                                    su.get_extent(mesh_left_half, 1),
+                                    1)
+
+    mesh_left_cond_thirds = su.split_into_thirds(
+        mesh_left_cond, 1)
+    mesh_right_cond_thirds = su.split_into_thirds(
+        mesh_right_cond, 1)
+    mesh_left_cond_thirds_x = su.split_into_thirds(
+        mesh_left_cond, 0)
+    mesh_right_cond_thirds_x = su.split_into_thirds(
+        mesh_right_cond, 0)
+    mesh_un_thirds = su.split_into_thirds(mesh_un, 1)
+
+    # mesh_un_middline = su.get_mid_array(mesh_un, 2, "y")
+    mesh_un_thirds_middline = []
+    for i in mesh_un_thirds:
+        mesh_un_thirds_middline.append(su.get_mid_array(i, 2, "y"))
+
+    mesh_left_cond_thirds_x_middline = []
+    for i in mesh_left_cond_thirds_x:
+        mesh_left_cond_thirds_x_middline.append(su.get_mid_array(i, 2, "x"))
+
+    mesh_left_cond_thirds_middline = []
+    for i in mesh_left_cond_thirds:
+        mesh_left_cond_thirds_middline.append(su.get_mid_array(i, 2, "y"))
+
+    mesh_right_cond_thirds_x_middline = []
+    for i in mesh_right_cond_thirds_x:
+        mesh_right_cond_thirds_x_middline.append(su.get_mid_array(i, 2, "x"))
+
+    mesh_right_cond_thirds_middline = []
+    for i in mesh_right_cond_thirds:
+        mesh_right_cond_thirds_middline.append(su.get_mid_array(i, 2, "y"))
+
     positions = [
-        # su.boundingorigin(mesh_un),
-        # su.boundingorigin(mesh_left_half),
-        # su.boundingorigin(mesh_right_half),
-        # su.boundingorigin(mesh_right_cond),
-        # su.boundingorigin(mesh_left_cond),
-        # su.boundingorigin(mesh_left_cond_thirds[0]),
-        # su.boundingorigin(mesh_left_cond_thirds[1]),
-        # su.boundingorigin(mesh_left_cond_thirds[2]),
-        # su.boundingorigin(mesh_right_cond_thirds[0]),
-        # su.boundingorigin(mesh_right_cond_thirds[1]),
-        # su.boundingorigin(mesh_right_cond_thirds[2]),
-        # su.boundingorigin(mesh_left_cond_thirds_x[0]),
-        # su.boundingorigin(mesh_left_cond_thirds_x[1]),
-        # su.boundingorigin(mesh_left_cond_thirds_x[2]),
-        # su.boundingorigin(mesh_right_cond_thirds_x[0]),
-        # su.boundingorigin(mesh_right_cond_thirds_x[1]),
-        # su.boundingorigin(mesh_right_cond_thirds_x[2]),
         su.boundingorigin(mesh_right_cond_thirds_middline[0]),
         su.boundingorigin(mesh_right_cond_thirds_middline[1]),
         su.boundingorigin(mesh_right_cond_thirds_middline[2])
     ]
     sizes = [
-        # su.boundingsize(mesh_un),
-        # su.boundingsize(mesh_left_half),
-        # su.boundingsize(mesh_right_half),
-        # su.boundingsize(mesh_right_cond),
-        # su.boundingsize(mesh_left_cond),
-        # su.boundingsize(mesh_left_cond_thirds[0]),
-        # su.boundingsize(mesh_left_cond_thirds[1]),
-        # su.boundingsize(mesh_left_cond_thirds[2]),
-        # su.boundingsize(mesh_right_cond_thirds[0]),
-        # su.boundingsize(mesh_right_cond_thirds[1]),
-        # su.boundingsize(mesh_right_cond_thirds[2]),
-        # su.boundingsize(mesh_left_cond_thirds_x[0]),
-        # su.boundingsize(mesh_left_cond_thirds_x[1]),
-        # su.boundingsize(mesh_left_cond_thirds_x[2]),
-        # su.boundingsize(mesh_right_cond_thirds_x[0]),
-        # su.boundingsize(mesh_right_cond_thirds_x[1]),
-        # su.boundingsize(mesh_right_cond_thirds_x[2])
         su.boundingsize(mesh_right_cond_thirds_middline[0]),
         su.boundingsize(mesh_right_cond_thirds_middline[1]),
         su.boundingsize(mesh_right_cond_thirds_middline[2])
     ]
-
-    get_measurements(mesh_un_middline, "FCW", 0)
-    get_measurements(mesh_un_thirds_middline[2], "FCWA", 0)
-    get_measurements(mesh_un_thirds_middline[0], "FCWP", 0)
-    get_measurements(mesh_left_cond_thirds_middline[1],    "Left FW", 0)
-    get_measurements(mesh_left_cond_thirds_middline[2],    "Left FWA", 0)
-    get_measurements(mesh_left_cond_thirds_middline[0],    "Left FWP", 0)
-    get_measurements(mesh_left_cond_thirds_x_middline[1],  "Left FL", 1)
-    get_measurements(mesh_left_cond_thirds_x_middline[2],  "Left FLM", 1)
-    get_measurements(mesh_left_cond_thirds_x_middline[0],  "Left FLL", 1)
-    get_measurements(mesh_right_cond_thirds_middline[1],   "Right FW", 0)
-    get_measurements(mesh_right_cond_thirds_middline[2],   "Right FWA", 0)
-    get_measurements(mesh_right_cond_thirds_middline[0],   "Right FWP", 0)
-    get_measurements(mesh_right_cond_thirds_x_middline[1], "Right FL", 1)
-    get_measurements(mesh_right_cond_thirds_x_middline[2], "Right FLM", 1)
-    get_measurements(mesh_right_cond_thirds_x_middline[0], "Right FLL", 1)
 
     # print("positions = " + str(positions))
     # print("sizes = " + str(sizes))
@@ -177,7 +208,19 @@ def get_measurements(mesh, name, axis):
     # get (and print) the measurements according to Elsner et al 2010
 
     measure = abs(su.get_extent(mesh, axis)[1] - su.get_extent(mesh, axis)[0])
-    print(name + ", " + str(measure))
+    string = name + ", " + str(measure) + "\n"
+    print(string)
+    return string
+
+
+def export_to_csv(stl_name, folder, string):
+    f = open(folder + "/" + stl_name[:-4] + ".csv", "w")
+    print(folder + "/" + stl_name[:-4] + ".csv")
+    full_string = ""
+    for i in string:
+        full_string = full_string + str(i)
+    f.write(full_string)
+    f.close()
 
 
 if __name__ == "__main__":
@@ -190,4 +233,5 @@ if __name__ == "__main__":
             print(filename)
             mesh_un = su.get_uniq_pc(filename)
             mesh_zeroed = su.reposition(mesh_un)
-            plot(mesh_zeroed, filename, folder)
+            # plot(mesh_zeroed, filename, folder)
+            get_mesh_parts(mesh_zeroed, filename, folder)
---
+++ bounding_plotter.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import stl_tools as su
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib as mpl
+import pandas as pd
+
+pointsize = 2
+alphaval = 0.5
+
+
+if __name__ == "__main__":
+    os.chdir(sys.argv[1])
+    fig = plt.figure(1)
+    filelist = []
+    allfilelist = sorted(os.listdir(sys.argv[1]))
+    for filename in allfilelist:
+        if filename.endswith(".csv"):
+            filelist.append(filename)
+
+    print(filelist)
+    # data to plot
+    n_groups = 8
+    mpl.rcParams.update({'font.size': 16})
+
+    frames = []
+
+    for f in filelist:
+        df = pd.read_csv(
+            sys.argv[1] + "/" + f,
+            header=None,
+            sep=',')
+
+        print(df)
+        df_transposed = df.T
+
+        new_header = df_transposed.iloc[0]  # grab the first row for the header
+        df_transposed = df_transposed[1:]  # take the data less the header row
+        df_transposed.columns = new_header  # set the header row as the df
+        # header
+
+        frames.append(df_transposed)
+
+    df_concated = pd.concat(frames)
+    print(df_concated)
+
+    x_pos = np.arange(len(df_concated.columns))
+
+    print(df_concated.mean(axis=0))
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, df_concated.mean(axis=0), yerr=df_concated.std(
+        axis=0), align='center', alpha=0.5, ecolor='black', capsize=10)
+    ax.set_ylabel('Length (mm)')
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(df_concated.columns)
+    ax.yaxis.grid(True)
+
+    # Save the figure and show
+    plt.tight_layout()
+    plt.savefig('bar_plot_with_error_bars.png')
+    plt.show()
+
+    # plt.show()
+    # plt.savefig('foss_05_1.png', dpi=None, facecolor='w', edgecolor='w',
+    #         orientation='landscape', papertype=None, format=None,
+    #         transparent=False, bbox_inches=None, pad_inches=1,
+    #         frameon=None)
